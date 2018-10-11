eae1ccb42005fdf1e74c852287f83646bcd97f28
“Alex <partin.alexander@gmail.com>
2018-10-11 14:50:56 -0500

    Updated plotting functions.

--- utils_ml.py
+++ utils_ml.py
@@ -96,14 +96,28 @@ def drop_samples_on_class_count(df, y, min_class_size=100):
 
 
 def plot_confusion_matrix(y_true, y_pred, labels=None, figsize=None, title=None,
+                          cmap=sns.light_palette(color='navy', n_colors=100),
                           savefig=False, img_name='confusion'):
     """ Create a confusion matrix for a classification results.
     Args:
+        y_true : numpy array of true labels
+        y_pred : numpy array of predictions
         labels : list of unique label names
     Returns:
         df_conf : df of confusion matrix
     https://matplotlib.org/gallery/images_contours_and_fields/image_annotated_heatmap.html
     """
+    # TODO: should be a shorter way to put this assertion, or reshape the input arrays to support multi-dim ...
+    if (y_true.ndim > 1) and (y_true.shape[1] > 1):
+        raise TypeError('`y_true` must be a 1-D array.')
+    else: 
+        y_true = y_true.reshape(-1,)
+    
+    if (y_pred.ndim > 1) and (y_pred.shape[1] > 1):
+        raise TypeError('`y_pred` must be a 1-D array.')
+    else: 
+        y_pred = y_pred.reshape(-1,)
+
     np_conf = confusion_matrix(y_true, y_pred)
     df_conf = pd.DataFrame(np_conf, index=labels, columns=labels)
 
@@ -115,11 +129,14 @@ def plot_confusion_matrix(y_true, y_pred, labels=None, figsize=None, title=None,
     fig, ax = plt.subplots(figsize=figsize)
     #sns.set(font_scale=2.0)
     sns.heatmap(df_conf, annot=True, annot_kws={"size": fontsize}, fmt='d',
-                linewidths=0.99, cmap='Greens', linecolor='white')
+                linewidths=0.99, cmap=cmap, linecolor='white', cbar_kws=dict(ticks=[]))
     ax.set_ylabel('True', fontsize=fontsize)
     ax.set_xlabel('Predicted', fontsize=fontsize)
-    ax.set_xticklabels(labels, fontsize=fontsize)
-    ax.set_yticklabels(labels, fontsize=fontsize)
+    if labels is not None:
+        ax.set_xticks(range(len(labels)))
+        ax.set_yticks(range(len(labels)))
+        ax.set_xticklabels(labels, fontsize=fontsize)
+        ax.set_yticklabels(labels, fontsize=fontsize)
     # fig.tight_layout()
 
     if title:
@@ -168,7 +185,7 @@ def plot_cor_heatmap(cor, value_range=[-1, 1], title=None, cmap='jet', figsize=N
     fontsize = 8
     if figsize is None:
         sc_x, sc_y = 0.5, 0.5
-        figsize = sc_x*cor.shape[1], sc_y*cor.shape[0]
+        figsize = sc_x * cor.shape[1], sc_y * cor.shape[0]
 
     fig, ax = plt.subplots(figsize=(sc_x * cor.shape[1], sc_y * cor.shape[0]))
     if full == True:
@@ -195,22 +212,24 @@ def plot_cor_heatmap(cor, value_range=[-1, 1], title=None, cmap='jet', figsize=N
     return fig
 
 
-def plot_rf_fi(rf_model, figsize=(8, 5), plot_direction='h', columns=None, max_cols=None, title=None):
+def plot_rf_fi(rf_model, figsize=(8, 5), plot_direction='h', columns=None, max_cols_plot=None,
+               color='g', title=None, errorbars=True):
     """ Plot feature importance from a random forest.
     Args:
         plot_direction : direction of the bars (`v` for vertical, `h` for hrozontal)
         columns : list of columns names (df.columns)
-        max_cols (int) : number of top most important features to plot
+        max_cols_plot (int) : number of top most important features to plot
     Returns:
         indices : all feature indices ordered by importance
         fig : handle for plt figure
     """
     fontsize=14
+    alpha=0.7
 
     importance = rf_model.feature_importances_
     std = np.std([tree.feature_importances_ for tree in rf_model.estimators_], axis=0)
     indices = np.argsort(importance)[::-1]  # feature indices ordered by importance
-    top_indices = indices[:max_cols]    # get indices of top most important features
+    top_indices = indices[:max_cols_plot]    # get indices of top most important features
     if columns is None:
         columns = top_indices
     else:
@@ -222,22 +241,33 @@ def plot_rf_fi(rf_model, figsize=(8, 5), plot_direction='h', columns=None, max_c
         ax.set_title(title)
         
     if plot_direction=='v':
-        ax.bar(range(len(top_indices)), importance[top_indices], yerr=std[top_indices], align='center', color='y', ecolor='black')
+        if errorbars:
+            ax.bar(range(len(top_indices)), importance[top_indices], color=color, align='center', alpha=alpha,
+                   yerr=std[top_indices], ecolor='black')
+        else:
+            ax.bar(range(len(top_indices)), importance[top_indices], color=color, align='center', alpha=alpha)
         ax.set_xticks(range(len(top_indices)))
-        ax.set_xticklabels(columns, rotation='vertical')
+        ax.set_xticklabels(columns, rotation='vertical', fontsize=fontsize)
         ax.set_xlim([-1, len(top_indices)])
         ax.set_xlabel('Feature', fontsize=fontsize)
         ax.set_ylabel('Importance', fontsize=fontsize)
+        [tick.label.set_fontsize(fontsize-4) for tick in ax.yaxis.get_major_ticks()]
     else:
-        ax.barh(range(len(top_indices)), importance[top_indices], xerr=std[top_indices], align='center', color='y', ecolor='black')
+        if errorbars:
+            ax.barh(range(len(top_indices)), importance[top_indices], color=color, align='center', alpha=alpha,
+                    xerr=std[top_indices], ecolor='black')
+        else:
+            ax.barh(range(len(top_indices)), importance[top_indices], color=color, align='center', alpha=alpha)
         ax.set_yticks(range(len(top_indices)))
-        ax.set_yticklabels(columns, rotation='horizontal')
+        ax.set_yticklabels(columns, rotation='horizontal', fontsize=fontsize)
         ax.set_ylim([-1, len(top_indices)])
         # ax.invert_yaxis()
         ax.set_ylabel('Feature', fontsize=fontsize)
         ax.set_xlabel('Importance', fontsize=fontsize)
+        [tick.label.set_fontsize(fontsize-4) for tick in ax.xaxis.get_major_ticks()]
 
-    ax.grid()
+    # ax.grid()
+    # plt.tight_layout()
 
     return indices, fig
 
